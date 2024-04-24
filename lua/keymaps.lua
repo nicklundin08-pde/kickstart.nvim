e2758c639d3088b0bf0326937f11fa7e02224727
Nick Lundin <nick@moontower.net>
2024-04-23 21:41:58 -0500

    Keymaps and color scheme

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -49,3 +49,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- vim: ts=2 sts=2 sw=2 et
+
+-- save file
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
